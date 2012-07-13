4b460814c2cdca0dadadff7823343fdbddc596e8
Dean Strelau <dean@mintdigital.com>
2012-07-13 18:14:23 -0400

    [007] optimize by only searching primes to sqrt of candidate

--- 007/007.clj
+++ 007/007.clj
@@ -2,7 +2,7 @@
   (loop [p [2] i 3]
     (cond
       (= (count p) n) (last p)
-      (some #(zero? (mod i %)) p) (recur p (+ i 2))
+      (some #(zero? (mod i %)) (take-while #(<= % (Math/sqrt i)) p)) (recur p (+ i 2))
       :else (recur (conj p i) (+ i 2)))))
 
 (println (prime 10001))
--- 007/007.rb
+++ 007/007.rb
@@ -2,7 +2,9 @@ def nth_prime(n)
   primes = [2]
   i = 3
   while primes.length < n
-    primes << i unless primes.any? {|p| i % p == 0 }
+    unless primes.any? {|p| break if p > Math.sqrt(i) ; i % p == 0 }
+      primes << i
+    end
     i += 2
   end
   primes[-1]
